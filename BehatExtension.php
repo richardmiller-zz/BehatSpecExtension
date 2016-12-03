12dff5fdc89ea7a4fc8f46a8633a83179057906a
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2016-12-03 15:46:48 +0100

    Switch BehatSpec to monorepo

---
+++ BehatExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace RMiller\BehatSpecExtension;
+
+use Behat\Testwork\ServiceContainer\Extension;
+use Behat\Testwork\ServiceContainer\ExtensionManager;
+use RMiller\ErrorExtension\ErrorExtension;
+use RMiller\PhpSpecExtension\PhpSpecExtension as BehatPhpSpecExtension;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class BehatExtension implements Extension
+{
+    private $extensions = [];
+
+    public function __construct()
+    {
+        $this->extensions = [
+            new BehatPhpSpecExtension(),
+            new ErrorExtension(),
+        ];
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        foreach ($this->extensions as $extension) {
+            $extension->process($container);
+        }
+    }
+
+    public function initialize(ExtensionManager $extensionManager)
+    {
+        foreach ($this->extensions as $extension) {
+            $extension->initialize($extensionManager);
+        }
+    }
+
+    public function load(ContainerBuilder $container, array $config)
+    {
+        foreach ($this->extensions as $extension) {
+            $extension->load($container, $config);
+        }
+    }
+
+    public function getConfigKey()
+    {
+        return 'behatspec';
+    }
+
+    public function configure(ArrayNodeDefinition $builder)
+    {
+        $builder
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('path')->defaultValue('bin/phpspec')->end()
+                ->scalarNode('config')->defaultNull()->end()
+            ->end()
+         ->end();
+    }
+}
---
+++ PhpSpecExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RMiller\BehatSpecExtension;
+
+use PhpSpec\Extension\ExtensionInterface;
+use PhpSpec\ServiceContainer;
+use RMiller\ExemplifyExtension\ExemplifyExtension;
+use RMiller\PhpSpecRunExtension\PhpSpecRunExtension;
+
+class PhpSpecExtension implements ExtensionInterface
+{
+    private $extensions = [];
+
+    public function __construct()
+    {
+        $this->extensions = [
+            new ExemplifyExtension(),
+            new PhpSpecRunExtension()
+        ];
+    }
+
+    /**
+     * @param ServiceContainer $container
+     */
+    public function load(ServiceContainer $container)
+    {
+        foreach ($this->extensions as $extension) {
+            $extension->load($container);
+        }
+
+        $params = $container->getParam('rerunner', []);
+        $params['commands'] = isset($params['commands']) ? $params['commands'] : ['describe', 'exemplify'];
+        $container->setParam('rerunner', $params);
+    }
+}
